45e0e516ecce6cbc1fb9056df635dacbb863a3eb
Giuliana <giuliana@MacBook-Air.local>
2025-10-09 11:09:40 -0300

    First scenario Cypress Automation

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,11 +2,11 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type('RandomTest 123')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').contains('RandomTest 123')
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
+      if ($element.text() === 'RandomTest 123') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
